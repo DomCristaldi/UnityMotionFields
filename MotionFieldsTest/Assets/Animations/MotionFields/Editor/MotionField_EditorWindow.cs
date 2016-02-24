4a1c276bb12b8b4d5758733747882d37b8cb2ee1
DomCristaldi <domeniccristaldi@gmail.com>
2016-02-23 22:29:45 -0500

    Initial Commit, got the reorderable list to work for a scirptable object

---
+++ MotionFieldsTest/Assets/Animations/MotionFields/Editor/MotionField_EditorWindow.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using System.Collections.Generic;
+
+
+public class MotionField_EditorWindow : EditorWindow {
+
+    [SerializeField]
+    public SO_MotionField selectedMotionField;
+
+    //[SerializeField]
+    //public List<AnimationClip> animClips;
+
+    private ReorderableList reorderableAnimClips;
+
+    //[SerializeField]
+
+    
+
+    [MenuItem("MotionFields/Motion Field Author")]
+    static void Init() {
+        MotionField_EditorWindow window = (MotionField_EditorWindow)EditorWindow.GetWindow(typeof(MotionField_EditorWindow));
+        window.Show();
+    }
+
+
+    void OnEnable() {
+        Debug.Log("Bloop");
+
+        //animClips = new List<AnimationClip>();
+
+        reorderableAnimClips = new ReorderableList(new List<AnimationClip>(), typeof(AnimationClip), true, true, true, true);
+
+    }
+
+
+    void OnGUI() {
+
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.BeginHorizontal();
+            
+            if (selectedMotionField != null) {
+                if (GUILayout.Button("Load Motion Field Data")) {
+                    LoadMotionField();
+                }
+            }
+
+            //OBJECT FIELD FOR THE MOTION FIELD
+            selectedMotionField = (SO_MotionField) EditorGUILayout.ObjectField("Motion Field: ", selectedMotionField, typeof(SO_MotionField), false);
+
+
+        EditorGUILayout.EndHorizontal();
+
+
+
+        if (selectedMotionField != null) {
+            reorderableAnimClips.DoLayoutList();
+        }
+
+        EditorGUILayout.EndVertical();
+
+
+    }
+
+
+    private void LoadMotionField() {
+        if (selectedMotionField != null) {
+            var serObj = new UnityEditor.SerializedObject(selectedMotionField);
+
+            //reorderableAnimClips = new ReorderableList(animClips, typeof(AnimationClip), true, true, true, true);
+            reorderableAnimClips = new ReorderableList(serObj, serObj.FindProperty("animClips"), true, true, true, true);
+        }
+        else {
+            Debug.LogWarning("Motion Field Author: No Assigned Motion Field Found");
+        }
+
+    }
+
+
+}
---
+++ MotionFieldsTest/Assets/Scripts/SO_MotionField.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditorInternal;
+#endif
+
+[System.Serializable]
+public class animClipInfo {
+    public bool useClip = true;
+    public AnimationClip animClip;
+}
+
+[CreateAssetMenu]
+public class SO_MotionField : ScriptableObject {
+
+    public List<animClipInfo> animClipInfoList;
+
+
+}
+
+
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(SO_MotionField))]
+public class SO_MotionField_Editor : Editor {
+
+    private ReorderableList reorderAnimList;
+
+
+    void OnEnable() {
+        reorderAnimList = new ReorderableList(serializedObject, serializedObject.FindProperty("animClipInfoList"), true, true, true, true);
+
+        reorderAnimList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) => {
+            var element = reorderAnimList.serializedProperty.GetArrayElementAtIndex(index);
+
+            //EditorGUILayout.BeginHorizontal();
+            //GUILayout.BeginArea(rect);
+
+            //EditorGUI.ObjectField(rect, element, typeof(AnimationClip));
+            
+            EditorGUI.PropertyField(new Rect(rect.x, rect.y, 60, EditorGUIUtility.singleLineHeight),
+                                    element.FindPropertyRelative("useClip"),
+                                    GUIContent.none);
+
+            EditorGUI.PropertyField(new Rect(rect.x + 60, rect.y, rect.width - 60, EditorGUIUtility.singleLineHeight),
+                                    element.FindPropertyRelative("animClip"),
+                                    GUIContent.none);
+
+            //EditorGUILayout.PropertyField(element.FindPropertyRelative("useClip"));
+            //EditorGUILayout.PropertyField(element.FindPropertyRelative("animClip"));
+
+
+            //EditorGUILayout.EndHorizontal();
+            //GUILayout.EndArea();
+
+        };
+
+    }
+
+
+    public override void OnInspectorGUI() {
+        //base.OnInspectorGUI();
+
+        serializedObject.Update();
+        reorderAnimList.DoLayoutList();
+        serializedObject.ApplyModifiedProperties();
+
+
+
+    }
+
+}
+#endif
