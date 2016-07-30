319bee81596751f2902c13df80f2b7c5b1a912ee
altobase <richpietrzak64@gmail.com>
2016-07-30 05:37:56 -0400

    changed ATask to be an abstract class with abstract methods.

--- MotionFieldsTest/Assets/Scripts/ATask.cs
+++ MotionFieldsTest/Assets/Scripts/ATask.cs
@@ -1,27 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ATask : ScriptableObject{
-
-	public ATask(){
-		
-	}
+public abstract class ATask : ScriptableObject{
 
 	public float max;
 
 	public float min;
 
 	public int numSamples;
-		
-	virtual public float CheckReward (MotionPose oldPose, MotionPose newPose, float taskval){
-		//get the immediate reward based on how beneficial the change in pose is for the taskval
-		return -1f;
-	}
 
-	virtual public float DetermineTaskValue (){
-		//decides the value of this task element. 
-        //task values determined by parameters in the world.
-		return -1f;
-	}
+    abstract public float CheckReward(MotionPose oldPose, MotionPose newPose, float taskval);
+
+    abstract public float DetermineTaskValue();
 		
 }
