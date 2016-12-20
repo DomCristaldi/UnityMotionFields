0b08d85ba5061a3901c858d492341d3da480eec6
rpietrzak256 <rpietrzak256@gmail.com>
2016-12-19 21:40:01 -0500

    Put plugins in proper directories

---
+++ MotionFieldsTest/Assets/WalkingTests/Tasks/Task_Trajectory.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu]
+public class Task_Trajectory : ATask
+{
+
+    public Task_Trajectory()
+    {
+
+    }
+
+    override public float CheckReward(MotionPose oldPose, MotionPose newPose, float taskval)
+    {
+        return 0;
+    }
+
+    override public float DetermineTaskValue()
+    {
+        return 0;
+    }
+}
